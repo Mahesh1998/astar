123dfabaaf7fb840c06afb553f84e53d2084bc78
Mahesh Maddineni <mahesh98@bali.x>
2024-05-15 18:05:57 -0600

    data processing in progress

---
+++ min_heap.h
@@ -0,0 +1,6 @@
+#ifndef MIN_HEAP_H_   /* Include guard */
+#define MIN_HEAP_H_
+
+int a;
+
+#endif
--- temp.c
+++ temp.c
@@ -6,8 +6,8 @@
 struct node{
     int x;
     int y;
-    char name[10];
-    char next[100][100];
+    char *name[10];
+    char *next[100];
 };
 
 // char** split_data(char* data, char delimiter){
@@ -15,23 +15,6 @@ struct node{
 //     char* result = malloc();
 //     while()
 // }
-
-int num_lines_in_file(char* filename){
-    FILE *f = fopen(filename, "r");
-    if (f == NULL){
-        perror("Unable to open file!");
-        exit(1);
-    }
-
-    char line[20];
-    int num_lines = 0;
-
-    while(fgets(line, sizeof(line), f) != NULL) {
-        num_lines++;
-    }
-    fclose(f);
-    return num_lines;
-}
 
 int main(){
     FILE *coordinates = fopen("input/input1/co-ordinates.txt", "r");
@@ -41,13 +24,16 @@ int main(){
     }
 
     char line[20];
-    int i =0, index = 0;
-    int num_lines = num_lines_in_file("input/input1/co-ordinates.txt");
-
-    struct node nodes[num_lines];
-
+    int i =0;
+    int num_lines = 0;
 
     while(fgets(line, sizeof(line), coordinates) != NULL) {
+        num_lines++;
+    }
+    fclose(coordinates);
+
+    while(fgets(line, sizeof(line), coordinates) != NULL) {
+        num_lines++;
         char *ptr;
         //split_data(line, ",");
         char node_data[10][10];
@@ -61,18 +47,6 @@ int main(){
             ptr = strtok( NULL, ",");
         }
 
-        strcpy(nodes[index].name, node_data[0]);
-        nodes[index].x = atoi(node_data[1]);
-        nodes[index].y = atoi(node_data[2]);
-        int next_index = 0;
-
-
-        for(int j = 3; j < i; j++){
-            strcpy(nodes[index].next[next_index++], node_data[j]);
-        }
-        index++;
-
-
         for(int j = 0; j < i; j++){
             printf("%s ", node_data[j]);
         }
@@ -80,16 +54,8 @@ int main(){
      }
      fclose(coordinates);
 
-     for (index = 0; index < num_lines; index++){
-         printf("%d  %d  %s  ", &nodes[index].x, &nodes[index].y, nodes[index].name);
-         int j = 0;
-         while(nodes[index].next[j]){
-             printf("%s  ", nodes[index].next[j++]);
-        }
-        printf("\n");
-    }
 
-
+     struct body bodies[n];
 
    /*  FILE *directions = fopen("input/input1/directions.txt", "r");
 
