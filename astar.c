8a1e309e2aef812ce158074daec2122299ad315a
Mahesh Maddineni <mahesh98@bali.x>
2024-05-17 15:58:01 -0600

    calculated priority

--- astar.c
+++ astar.c
@@ -1,24 +1,106 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include "min_heap.h"
+#include <malloc.h>
+#include <math.h>
 
-struct node{
+#define MAX_NEIGHBOR_NODES 100
+#define MAX_NODES 100
+
+/***************Variables***************/
+struct astar_node
+{
     int x;
     int y;
-    char name[10];
-    char next[100][100];
+    char *name;
+    int neighbors_count;
+    struct astar_node* neighbor[MAX_NEIGHBOR_NODES];
 };
 
-// char** split_data(char* data, char delimiter){
-//     static
-//     char* result = malloc();
-//     while()
-// }
+struct astar_graph
+{
+    struct astar_node* nodes[MAX_NODES];
+    int count;
+};
 
-int num_lines_in_file(char* filename){
+struct astar_path
+{
+    struct astar_node* node;
+    double distance;
+    double heuristic_distance;
+    struct astar_path *parent;
+};
+
+struct astar_graph graph = {.count = 0 };
+
+
+
+/***************Function Declarations***************/
+int num_lines_in_file(char *filename);
+void read_input(int num_lines, char nodes_file[], char neighbor_file[]);
+void check_source_destination_exists(char *source, char *destination, struct astar_node* nodes);
+void astar(char *source, char *destination, struct astar_node* nodes);
+struct astar_node* get_node(char *name);
+void add_graph_node(char *name, int x, int y);
+void add_neighbor_node(char *node_name, char *neighbor_name);
+
+
+
+/***************Function Definitions***************/
+struct astar_node* get_node(char *name)
+{
+    /*printf("finding %s \n", name);*/
+    for (int i = 0; i < graph.count; i++) {
+        /*printf("%s %s %d %d\n", graph.nodes[i]->name, name, strcmp(graph.nodes[i]->name, name), graph.nodes[i]->x);*/
+        if (strcmp(graph.nodes[i]->name, name) == 0) {
+            return graph.nodes[i];
+        }
+    }
+    return NULL;
+}
+
+void add_graph_node(char *name, int x, int y)
+{
+    struct astar_node *node= (struct astar_node*) malloc(sizeof(struct astar_node));
+
+    node->name = (char *) malloc(strlen(name) + 1);
+
+    strcpy(node->name, name);
+    node->x = x;
+    node->y = y;
+    node->neighbors_count = 0;
+    for (int i = 0; i < MAX_NEIGHBOR_NODES; i++) {
+        node->neighbor[i] = NULL;
+    }
+    graph.nodes[graph.count++] = node;
+}
+
+void add_neighbor_node(char *node_name, char *neighbor_name)
+{
+    struct astar_node *node= get_node(node_name);
+
+    if (!node) {
+        perror("Unable to find node, while adding neighbor");
+        exit(1);
+    }
+    struct astar_node *neighbor = get_node(neighbor_name);
+
+    if (!neighbor) {
+        perror("Unable to find neighbor node, while adding neighbor");
+        exit(1);
+    }
+
+    node->neighbor[node->neighbors_count] = neighbor;
+    /*printf("printing neighbor %d %s %s\n", node->neighbors_count, node->name, node->neighbor[node->neighbors_count]->name);*/
+    node->neighbors_count++;
+}
+
+
+int get_num_lines(char *filename)
+{
     FILE *f = fopen(filename, "r");
-    if (f == NULL){
+
+    if (f == NULL) {
         perror("Unable to open file!");
         exit(1);
     }
@@ -26,100 +108,157 @@ int num_lines_in_file(char* filename){
     char line[20];
     int num_lines = 0;
 
-    while(fgets(line, sizeof(line), f) != NULL) {
+    while (fgets(line, sizeof(line), f) != NULL) {
         num_lines++;
     }
     fclose(f);
     return num_lines;
 }
 
-int main(){
-    FILE *coordinates = fopen("input/input1/co-ordinates.txt", "r");
-    if (coordinates == NULL){
+void read_input(int num_lines, char *nodes_file, char *neighbor_file)
+{
+    FILE *coordinates = fopen(nodes_file, "r");
+
+    if (coordinates == NULL) {
         perror("Unable to open file!");
         exit(1);
     }
 
     char line[20];
-    int i =0, index = 0;
-    int num_lines = num_lines_in_file("input/input1/co-ordinates.txt");
+    int i = 0, index = 0;
 
-    struct node nodes[num_lines];
-
-
-    while(fgets(line, sizeof(line), coordinates) != NULL) {
+    while (fgets(line, sizeof(line), coordinates) != NULL) {
+        line[strcspn(line, "\r\n")] = '\0';
         char *ptr;
+
         //split_data(line, ",");
         char node_data[10][10];
-        ptr =  strtok(line, ",");
+
+        ptr = strtok(line, ",");
         i = 0;
-        while(ptr != NULL){
-            //node_data[i] = ptr;
+        while (ptr != NULL) {
             strcpy(node_data[i], ptr);
             //printf("%s (ptr) %s (node_data)\n", ptr, node_data[i]);
             i++;
-            ptr = strtok( NULL, ",");
+            ptr = strtok(NULL, ",");
         }
 
-        strcpy(nodes[index].name, node_data[0]);
-        nodes[index].x = atoi(node_data[1]);
-        nodes[index].y = atoi(node_data[2]);
-        int next_index = 0;
-
-
-        for(int j = 3; j < i; j++){
-            strcpy(nodes[index].next[next_index++], node_data[j]);
-        }
+        add_graph_node(node_data[0], atoi(node_data[1]), atoi(node_data[2]));
         index++;
 
+    }
+    fclose(coordinates);
 
-        for(int j = 0; j < i; j++){
-            printf("%s ", node_data[j]);
+
+    FILE *directions = fopen(neighbor_file, "r");
+
+    while (fgets(line, sizeof(line), directions) != NULL) {
+        line[strcspn(line, "\r\n")] = '\0';
+        char *ptr;
+
+        //split_data(line, ",");
+        char node_data[10][10];
+
+        ptr = strtok(line, ",");
+        i = 0;
+        while (ptr != NULL) {
+            strcpy(node_data[i], ptr);
+            //printf("%s (ptr) %s (node_data)\n", ptr, node_data[i]);
+            i++;
+            ptr = strtok(NULL, ",");
         }
-        printf("\n\n");
-     }
-     fclose(coordinates);
 
-     for (index = 0; index < num_lines; index++){
-         printf("%d  %d  %s  ", &nodes[index].x, &nodes[index].y, nodes[index].name);
-         int j = 0;
-         while(nodes[index].next[j]){
-             printf("%s  ", nodes[index].next[j++]);
+        add_neighbor_node(node_data[0], node_data[1]);
+        index++;
+
+    }
+
+    fclose(directions);
+
+    for (index = 0; index < graph.count; index++) {
+        printf("%d  %d  %s  %d ",
+                graph.nodes[index]->x,
+                graph.nodes[index]->y,
+                graph.nodes[index]->name,
+                graph.nodes[index]->neighbors_count);
+
+
+
+        for(int j = 0; j < graph.nodes[index]->neighbors_count; j++){
+            printf("%s ", graph.nodes[index]->neighbor[j]->name);
         }
         printf("\n");
     }
 
+}
 
+double euclidean_distance(struct astar_node* a, struct astar_node* b) {
+    return sqrt((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y));
+}
 
-   /*  FILE *directions = fopen("input/input1/directions.txt", "r");
+struct astar_path* create_a_star_node(struct astar_node* node, double distance, double heuristic_distance, struct astar_path* parent) {
+    struct astar_path* a_star_node = (struct astar_path *)malloc(sizeof(struct astar_path));
+    a_star_node->node = node;
+    a_star_node->distance = distance;
+    a_star_node->heuristic_distance = heuristic_distance;
+    a_star_node->parent = parent;
+    return a_star_node;
+}
 
-     if(directions == NULL) {
-         perror("Unable to open file!");
-         exit(1);
+void a_star(struct astar_node* source, struct astar_node* destination) {
+    struct astar_path* open_list[MAX_NODES];
+    int open_list_count = 0;
+
+    open_list[open_list_count++] = create_a_star_node(source, 0, euclidean_distance(source, destination), NULL);
+
+    printf("Open List\n");
+
+    for(int i = 0; i< open_list_count; i++){
+        printf("%d  %d  %s  %d %lf %lf\n",
+                open_list[i]->node->x,
+                open_list[i]->node->y,
+                open_list[i]->node->name,
+                open_list[i]->node->neighbors_count,
+                open_list[i]->distance,
+                open_list[i]->heuristic_distance);
     }
 
+    while (open_list_count > 0) {
+        int current_index = 0;
 
-    while(fgets(line, sizeof(line), directions) != NULL) {
-         char *ptr;
-        //split_data(line, ",");
-        char node_data[10][10];
-        ptr =  strtok(line, ",");
-        i = 0;
-        while(ptr != NULL){
-            //node_data[i] = ptr;
-            strcpy(node_data[i], ptr);
-            //printf("%s (ptr) %s (node_data)\n", ptr, node_data[i]);
-            i++;
-            ptr = strtok( NULL, ",");
+        struct astar_path* current = open_list[current_index];
+        if (current->node->name == destination->name) {
+            printf("Path found: ");
+            while (current) {
+                printf("%s ", current->node->name);
+                current = current->parent;
+            }
+            printf("\n");
+            return;
         }
 
-        for(int j = 0; j < i; j++){
-            printf("%s ", node_data[j]);
+        printf("Priority of neighbor nodes of source node\n");
+
+        for (int i = 0; i < current->node->neighbors_count; i++) {
+            struct astar_node* neighbor = current->node->neighbor[i];
+
+            double priority = euclidean_distance(current->node, neighbor)+ euclidean_distance(neighbor, destination);
+            printf("%lf\n", priority);
         }
-     }*/
+        break;
+
+    }
+
+}
 
 
-
-   // fclose(directions);
+int main(int argc, char *argv[])
+{
+    int num_lines = get_num_lines(argv[1]);
+    read_input(num_lines, argv[1], argv[2]);
+    char *source = argv[3];
+    char *destination = argv[4];
+    a_star(get_node(source), get_node(destination));
     return 0;
+
 }
--- temp.c
+++ temp.c
@@ -1,91 +1,164 @@
-#include<stdio.h>
-#include<string.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include "min_heap.h"
+#include <math.h>
+#include <string.h>
 
-struct node{
-    int x;
-    int y;
-    char *name[10];
-    char *next[100];
-};
+#define MAX_NODES 100
+#define INF 1000000
 
-// char** split_data(char* data, char delimiter){
-//     static
-//     char* result = malloc();
-//     while()
-// }
+typedef struct Node {
+    char name[3];
+    int x, y;
+    int neighbors_count;
+    struct Node* neighbors[MAX_NODES];
+} Node;
 
-int main(){
-    FILE *coordinates = fopen("input/input1/co-ordinates.txt", "r");
-    if (coordinates == NULL){
-        perror("Unable to open file!");
-        exit(1);
+typedef struct {
+    Node* nodes[MAX_NODES];
+    int count;
+} Graph;
+
+typedef struct {
+    Node* node;
+    double cost;
+    double heuristic;
+    struct AStarNode* parent;
+} AStarNode;
+
+Graph graph = { .count = 0 };
+
+Node* find_node(char* name) {
+    for (int i = 0; i < graph.count; i++) {
+        if (strcmp(graph.nodes[i]->name, name) == 0) {
+            return graph.nodes[i];
+        }
+    }
+    return NULL;
+}
+
+void add_node(char* name, int x, int y) {
+    Node* node = (Node*)malloc(sizeof(Node));
+    strcpy(node->name, name);
+    node->x = x;
+    node->y = y;
+    node->neighbors_count = 0;
+    graph.nodes[graph.count++] = node;
+}
+
+void add_neighbor(char* node_name, char* neighbor_name) {
+    Node* node = find_node(node_name);
+    Node* neighbor = find_node(neighbor_name);
+    if (node && neighbor) {
+        node->neighbors[node->neighbors_count++] = neighbor;
+    }
+}
+
+double heuristic(Node* a, Node* b) {
+    return sqrt((a->x - b->x) * (a->x - b->x) + (a->y - b->y) * (a->y - b->y));
+}
+
+AStarNode* create_a_star_node(Node* node, double cost, double heuristic, AStarNode* parent) {
+    AStarNode* a_star_node = (AStarNode*)malloc(sizeof(AStarNode));
+    a_star_node->node = node;
+    a_star_node->cost = cost;
+    a_star_node->heuristic = heuristic;
+    a_star_node->parent = parent;
+    return a_star_node;
+}
+
+void a_star(Node* start, Node* goal) {
+    AStarNode* open_list[MAX_NODES];
+    int open_list_count = 0;
+    int closed_list[MAX_NODES] = {0};
+
+    open_list[open_list_count++] = create_a_star_node(start, 0, heuristic(start, goal), NULL);
+
+    while (open_list_count > 0) {
+        int current_index = 0;
+        for (int i = 1; i < open_list_count; i++) {
+            if (open_list[i]->cost + open_list[i]->heuristic < open_list[current_index]->cost + open_list[current_index]->heuristic) {
+                current_index = i;
+            }
+        }
+
+        AStarNode* current = open_list[current_index];
+        if (current->node == goal) {
+            printf("Path found: ");
+            while (current) {
+                printf("%s ", current->node->name);
+                current = current->parent;
+            }
+            printf("\n");
+            return;
+        }
+
+        for (int i = current_index; i < open_list_count - 1; i++) {
+            open_list[i] = open_list[i + 1];
+        }
+        open_list_count--;
+
+        closed_list[find_node(current->node->name) - graph.nodes[0]] = 1;
+
+        for (int i = 0; i < current->node->neighbors_count; i++) {
+            Node* neighbor = current->node->neighbors[i];
+            if (closed_list[neighbor - graph.nodes[0]]) {
+                continue;
+            }
+
+            double tentative_cost = current->cost + heuristic(current->node, neighbor);
+            int in_open_list = 0;
+            for (int j = 0; j < open_list_count; j++) {
+                if (open_list[j]->node == neighbor) {
+                    in_open_list = 1;
+                    if (tentative_cost < open_list[j]->cost) {
+                        open_list[j]->cost = tentative_cost;
+                        open_list[j]->parent = current;
+                    }
+                    break;
+                }
+            }
+
+            if (!in_open_list) {
+                open_list[open_list_count++] = create_a_star_node(neighbor, tentative_cost, heuristic(neighbor, goal), current);
+            }
+        }
     }
 
-    char line[20];
-    int i =0;
-    int num_lines = 0;
+    printf("No path found\n");
+}
 
-    while(fgets(line, sizeof(line), coordinates) != NULL) {
-        num_lines++;
-    }
-    fclose(coordinates);
+int main() {
+    // Add nodes
+    add_node("s", 0, 0);
+    add_node("a1", 2, 1);
+    add_node("a2", 2, 2);
+    add_node("a3", 2, 3);
+    add_node("b1", 4, 1);
+    add_node("b2", 4, 2);
+    add_node("b3", 4, 3);
+    add_node("b4", 4, 4);
+    add_node("c1", 6, 1);
+    add_node("d", 8, 2);
 
-    while(fgets(line, sizeof(line), coordinates) != NULL) {
-        num_lines++;
-        char *ptr;
-        //split_data(line, ",");
-        char node_data[10][10];
-        ptr =  strtok(line, ",");
-        i = 0;
-        while(ptr != NULL){
-            //node_data[i] = ptr;
-            strcpy(node_data[i], ptr);
-            //printf("%s (ptr) %s (node_data)\n", ptr, node_data[i]);
-            i++;
-            ptr = strtok( NULL, ",");
-        }
+    // Add neighbors
+    add_neighbor("s", "a1");
+    add_neighbor("s", "a2");
+    add_neighbor("s", "a3");
+    add_neighbor("a1", "b1");
+    add_neighbor("a2", "b2");
+    add_neighbor("a3", "b3");
+    add_neighbor("a3", "b4");
+    add_neighbor("b1", "c1");
+    add_neighbor("b2", "d");
 
-        for(int j = 0; j < i; j++){
-            printf("%s ", node_data[j]);
-        }
-        printf("\n\n");
-     }
-     fclose(coordinates);
-
-
-     struct body bodies[n];
-
-   /*  FILE *directions = fopen("input/input1/directions.txt", "r");
-
-     if(directions == NULL) {
-         perror("Unable to open file!");
-         exit(1);
+    // Execute A* algorithm
+    Node* start = find_node("s");
+    Node* goal = find_node("d");
+    if (start && goal) {
+        a_star(start, goal);
+    } else {
+        printf("Start or goal node not found\n");
     }
 
-
-    while(fgets(line, sizeof(line), directions) != NULL) {
-         char *ptr;
-        //split_data(line, ",");
-        char node_data[10][10];
-        ptr =  strtok(line, ",");
-        i = 0;
-        while(ptr != NULL){
-            //node_data[i] = ptr;
-            strcpy(node_data[i], ptr);
-            //printf("%s (ptr) %s (node_data)\n", ptr, node_data[i]);
-            i++;
-            ptr = strtok( NULL, ",");
-        }
-
-        for(int j = 0; j < i; j++){
-            printf("%s ", node_data[j]);
-        }
-     }*/
-
-
-
-   // fclose(directions);
     return 0;
 }
